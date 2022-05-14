6ab4565dac56ab74eec94ed7a04b81313519a7c5
Luan Guerra <luandevguerra@gmail.com>
2022-05-14 20:03:13 -0300

    fix tests

--- src/test/java/me/luandevguerra/rentacar/api/integration/cars/CarsStepsDef.java
+++ src/test/java/me/luandevguerra/rentacar/api/integration/cars/CarsStepsDef.java
@@ -3,6 +3,7 @@ package me.luandevguerra.rentacar.api.integration.cars;
 import io.cucumber.java.en.Given;
 import io.cucumber.java8.En;
 import me.luandevguerra.rentacar.api.models.Car;
+import me.luandevguerra.rentacar.api.repository.CarsRepository;
 import me.luandevguerra.rentacar.api.service.CarsService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -14,10 +15,19 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.List;
+
+import static me.luandevguerra.rentacar.api.utils.IntegrationContextData.getContextData;
+import static me.luandevguerra.rentacar.api.utils.IntegrationContextData.setContextData;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
 public class CarsStepsDef implements En {
+
+    @Autowired
+    private CarsRepository repository;
 
     @LocalServerPort
     private int port;
@@ -32,12 +42,20 @@ public class CarsStepsDef implements En {
 
     public CarsStepsDef() {
 
-        Given("I have two cars at the database", () -> {
-
+        Given("I have {long} cars at the database", (Long numberOfCars) -> {
+            for (int i = 0; i < numberOfCars; i++) {
+                repository.save(Car.builder().name("GOL").licensePlate("JUC-7358").build());
+            }
         });
 
         When("I call the resource to obtain all cars", () -> {
-            ResponseEntity<Car[]> forEntity = restTemplate.getForEntity("http://localhost:" + port + "/api/v1/cars", Car[].class);
+            ResponseEntity<Car[]> response = restTemplate.getForEntity("http://localhost:" + port + "/api/v1/cars", Car[].class);
+            setContextData("responseListSize", response.getBody().length);
+        });
+
+        Then("The car list received should have {numberOfTheCarsExpected} cars", (Long numberOfTheCarsExpected) -> {
+            Object responseListSize = getContextData("responseListSize");
+            assertEquals(numberOfTheCarsExpected, responseListSize);
         });
 
     }
--- src/test/resources/features/cars.feature
+++ src/test/resources/features/cars.feature
@@ -1,5 +1,10 @@
 Feature: Shopping
 
-  Scenario: Get all cars at the database
-    Given I have two cars at the database
+  Scenario Outline: Get all cars at the database
+    Given I have <numberOfTheCars> cars at the database
     When I call the resource to obtain all cars
+    Then The car list received should have {numberOfTheCarsExpected} cars
+
+    Examples:
+      | numberOfTheCars | numberOfTheCarsExpected |
+      | 2               | 2                       |
