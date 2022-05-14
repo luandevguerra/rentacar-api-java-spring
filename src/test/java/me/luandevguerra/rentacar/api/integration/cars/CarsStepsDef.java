ea5cfa32b198dd43325151d56e55e2eb8e27c632
Luan Guerra <luandevguerra@gmail.com>
2022-05-14 19:08:09 -0300

    fix to do the test works well

--- src/test/java/me/luandevguerra/rentacar/api/integration/cars/CarsStepsDef.java
+++ src/test/java/me/luandevguerra/rentacar/api/integration/cars/CarsStepsDef.java
@@ -3,28 +3,44 @@ package me.luandevguerra.rentacar.api.integration.cars;
 import io.cucumber.java.en.Given;
 import io.cucumber.java8.En;
 import me.luandevguerra.rentacar.api.models.Car;
+import me.luandevguerra.rentacar.api.repository.CarsRepository;
 import me.luandevguerra.rentacar.api.service.CarsService;
 import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
+
+import static me.luandevguerra.rentacar.api.utils.IntegrationContextData.resetData;
+import static me.luandevguerra.rentacar.api.utils.IntegrationContextData.getContextData;
+import static me.luandevguerra.rentacar.api.utils.IntegrationContextData.setContextData;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 @ActiveProfiles("test")
 public class CarsStepsDef implements En {
 
+    @Autowired
+    private CarsRepository repository;
+
     @LocalServerPort
     private int port;
     private final RestTemplate restTemplate = new RestTemplate();
 
     private String getPath() {
-        return "http://localhost:" + port +"/api/v1/cars";
+        return "http://localhost:" + port + "/api/v1/cars";
     }
 
     @Autowired
@@ -32,14 +48,23 @@ public class CarsStepsDef implements En {
 
     public CarsStepsDef() {
 
-        Given("I have two cars at the database", () -> {
-
+        Given("I have {int} cars at the database", (Integer numberOfCars) -> {
+            for (int i = 0; i < numberOfCars; i++) {
+                repository.save(Car.builder().name("GOL").licensePlate("JUC-7358").build());
+            }
         });
 
         When("I call the resource to obtain all cars", () -> {
-            ResponseEntity<Car[]> forEntity = restTemplate.getForEntity("http://localhost:" + port + "/api/v1/cars", Car[].class);
+            ResponseEntity<List<Car>> response = restTemplate.exchange(
+                    getPath(),  HttpMethod.GET, null, new ParameterizedTypeReference<>() {}
+            );
+
+            setContextData("Response", response);
         });
 
+        Then("The car list received should have {int} cars", (Integer numberOfCars) -> {
+            ResponseEntity<List<Car>> response = (ResponseEntity<List<Car>>) getContextData("Response");
+            assertEquals(numberOfCars, response.getBody().size());
+        });
     }
-
 }
